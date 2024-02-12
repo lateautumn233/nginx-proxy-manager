822f449cc75ff880ab1b1b7d0783e07035f0fe4b
jerry-yuan <i@jerryzone.cn>
2024-02-12 20:08:35 +0800

    i18n: Split translation file

--- frontend/js/app/i18n.js
+++ frontend/js/app/i18n.js
@@ -1,5 +1,4 @@
-const Cache    = ('./cache');
-const messages = require('../i18n/messages.json');
+const Cache = require('./cache');
 
 /**
  * @param {String}  namespace
@@ -8,16 +7,33 @@ const messages = require('../i18n/messages.json');
  */
 module.exports = function (namespace, key, data) {
     let locale = Cache.locale;
-    // check that the locale exists
+    if (locale == "zh-CN") locale = "zh-Hans" 
+    if (locale == "zh-TW") locale = "zh-Hant"
+    let messages = {};
+
+    // Load English translations
+    const enMessages = require('../i18n/messages.json');
+    messages = { ...messages, ...enMessages };
+
+    let localizedMessages = {};
+
+    // Load localized translations if available, otherwise fallback to English
+    try {
+        localizedMessages = require(`../i18n/${locale}/messages.json`);
+        messages = Object.assign(messages, localizedMessages);
+        console.log(messages);
+    } catch (error) {
+        console.error(`Failed to load ${locale} messages, falling back to English.`);
+    }
+
+    // Check that the locale exists
     if (typeof messages[locale] === 'undefined') {
         locale = 'en';
     }
 
-    if (typeof messages[locale][namespace] !== 'undefined' && typeof messages[locale][namespace][key] !== 'undefined') {
-        return messages[locale][namespace][key](data);
-    } else if (locale !== 'en' && typeof messages['en'][namespace] !== 'undefined' && typeof messages['en'][namespace][key] !== 'undefined') {
-        return messages['en'][namespace][key](data);
+    if (typeof messages[namespace] !== 'undefined' && typeof messages[namespace][key] !== 'undefined') {
+        return messages[namespace][key](data);
+    } else {
+        return '(MISSING: ' + namespace + '/' + key + ')';
     }
-
-    return '(MISSING: ' + namespace + '/' + key + ')';
 };
